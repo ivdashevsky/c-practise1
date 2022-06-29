3f6d02fa738afc537e4cc7d7daa8f7cf7339995e
Ilya <iii@ya.ru>
2022-06-29 12:05:05 +0500

    task 6 created

---
+++ Num6/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число");
+double a = Convert.ToDouble(Console.ReadLine());
+double res = a % 2;
+if (res == 0) {Console.WriteLine("Число чётное");}
+else {Console.WriteLine("Число нечётное");}
