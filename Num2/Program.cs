ec499680fdb5717dd2bcdd9d909458f199886792
Ilya <iii@ya.ru>
2022-06-29 12:18:17 +0500

    task 8 creation

--- Num2/Program.cs
+++ Num2/Program.cs
@@ -11,37 +11,3 @@ Console.Write("Максимальное число равно ");
 Console.WriteLine(max);
 Console.Write("Минимальное число равно ");
 Console.WriteLine(min);
-/*
-int a1 = 5, b1 = 7,
-    a2 = 2, b2 = 10,
-    a3 = -9, b3 = -3;
-
-Console.WriteLine("Список чисел 1");
-Console.Write("a = "); Console.WriteLine(a1);
-Console.Write("b = "); Console.WriteLine(b1);
-int max = a1;
-if (a1>b1) {max = a1;}
-else {max = b1;}
-Console.Write("Максимальное число равно ");
-Console.WriteLine(max);
-Console.WriteLine();
-
-Console.WriteLine("Список чисел 2");
-Console.Write("a = "); Console.WriteLine(a2);
-Console.Write("b = "); Console.WriteLine(b2);
-max = a2;
-if (a2>b2) {max = a2;}
-else {max = b2;}
-Console.Write("Максимальное число равно ");
-Console.WriteLine(max);
-Console.WriteLine();
-
-Console.WriteLine("Список чисел 3");
-Console.Write("a = "); Console.WriteLine(a3);
-Console.Write("b = "); Console.WriteLine(b3);
-max = a3;
-if (a3>b3) {max = a3;}
-else {max = b3;}
-Console.Write("Максимальное число равно ");
-Console.WriteLine(max);
-*/
---
+++ Num8/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int c = 1;
+Console.WriteLine("Список чётных чисел");
+while (c <= N)
+{
+    if (c % 2 == 0)
+    {
+        Console.WriteLine(c);
+    }
+    c++;
+}
